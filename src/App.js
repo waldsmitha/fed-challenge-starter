cc90c07a551771b51f172092f7633f2027127daa
Austin Waldsmith <austin.waldsmith@gmail.com>
2021-03-04 19:46:52 -0500

    basic setup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+
+import "./App.css";
+
+//Components
+import SeriesCard from "./components/SeriesCard";
+import VideoCard from "./components/VideoCard";
+
+//Styling
+import styled from "styled-components";
+import { motion } from "framer-motion";
+
+//Data
+import data from "./data";
 
 function App() {
+  const [active, setActive] = useState("false");
+  const [cardData, setCardData] = useState(data);
+  console.log(cardData);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <StyledSection>
+      <VideoCard data={cardData} />
+      <SeriesCard data={cardData} />
+      <VideoCard data={cardData} />
+      <SeriesCard data={cardData} />
+      <VideoCard data={cardData} />
+      <SeriesCard data={cardData} />
+      <VideoCard data={cardData} />
+      <SeriesCard data={cardData} />
+    </StyledSection>
   );
 }
+
+const StyledSection = styled(motion.div)`
+  display: grid;
+  min-height: 100vh;
+  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+  grid-column-gap: 2rem;
+  grid-row-gap: 2rem;
+  max-width: 1500px;
+`;
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,40 @@
+import lakeImgThumb from "./img/lake-inniscarra-thumb.jpg";
+import lakeImgTrainer from "./img/lake-inniscarra-trainer.jpg";
+
+let data = [
+  {
+    type: "video",
+    title: "Lake Inniscarra, Ireland - Pyramid",
+    time: "30:53",
+    views: "6,248 M",
+    img: [lakeImgThumb, lakeImgTrainer],
+  },
+  { type: "series", title: "Performance Series", img: lakeImgThumb },
+  {
+    type: "video",
+    title: "Slow Pulls and Fast Intervals",
+    time: "44:13",
+    views: "9,948 M",
+    img: [lakeImgThumb, lakeImgTrainer],
+  },
+
+  { type: "series", title: "20 Minutes to Toned", img: lakeImgThumb },
+  {
+    type: "video",
+    title: "Charles Race, Boston, Massachusetts",
+    time: "36:22",
+    views: "8,648 M",
+    img: [lakeImgThumb, lakeImgTrainer],
+  },
+  { type: "series", title: "Full-Body HIIT Series", img: lakeImgThumb },
+  {
+    type: "video",
+    title: "Kafue River, Zambia - Power Stroke Pyramid",
+    time: "22:22",
+    views: "4,660 M",
+    img: [lakeImgThumb, lakeImgTrainer],
+  },
+  { type: "series", title: "Shred & Burn Series", img: lakeImgThumb },
+];
+
+export default data;
